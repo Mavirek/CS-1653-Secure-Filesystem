e2f0100e14b3bea80bc54c969d008e8c444472c5
Allen Poon <alp170@pitt.edu>
2017-09-20 21:32:12 -0400

    updated FileClientApp

--- src/FileClientApp.java
+++ src/FileClientApp.java
@@ -18,6 +18,17 @@ public class FileClientApp
 			System.out.println("Connected to Group Server: "+args[1]+" Port: "+args[3]+" File Server: "+args[2]+" Port: "+args[4]);
 			System.out.print("Please enter the group name: ");
 			String groupName = s.next();
+			//check if group exists
+			System.out.println("Options:");
+			System.out.println("GET - get a token");
+			System.out.println("CUSER - create a user");
+			System.out.println("DUSER - delete a user");
+			System.out.println("CGROUP - create a group");
+			System.out.println("DGROUP - delete a group");
+			System.out.println("LMEMBERS - list members of a group");
+			System.out.println("AUSERTOGROUP - add a user to a group");
+			System.out.println("RUSERFROMGROUP - remove a user from a group");
+			System.out.println("DISCONNECT - disconnect from group server");
 
 		}
 		else
